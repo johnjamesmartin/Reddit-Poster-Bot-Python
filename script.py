24bf93039542901a49f7a7cd7b246d8c67884efd
John Martin <johnjamesmartin@live.com>
2019-07-25 01:57:10 +0100

    Add error handling

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ reddit = praw.Reddit(client_id='', client_secret='', user_agent='<PLATFORM:APPNA
 
 subreddits = ['programmerhumor', 'funny']
 pos = 0
+errors = 0
 
 title="Remember to hug a programmer"
 url="http://worklad.co.uk/wp-content/uploads/2015/01/494692340292596844.jpg"
@@ -13,16 +14,34 @@ url="http://worklad.co.uk/wp-content/uploads/2015/01/494692340292596844.jpg"
 def post():
 	global subreddits
 	global pos
+  global errors
 
-	subreddit = reddit.subreddit(subreddits[pos])
-	subreddit.submit(title, url=url)
+  try:
+    subreddit = reddit.subreddit(subreddits[pos])
+    subreddit.submit(title, url=url)
+    print("Posted to " + subreddits[posts])
+    pos = pos + 1
 
-	pos = pos + 1
-
-	if (pos >= len(subreddits) - 1):
-		post()
-	else:
-		print "Done"
-	post()
-
+    if (pos >= len(subreddits) - 1):
+      post()
+    else:
+      print "Done"
+    post()
+  except praw.exceptions.APIException as e:
+    if (e.error_type == 'RATELIMIT'):
+      delay = re.search("(\d+) minutes?", e.message)
+        if delay:
+          delay_seconds = float(int(delay.group(1)) * 60)
+          time.sleep(delay_seconds)
+          post()
+        else:
+          delay + re.search("(\d+) seconds", e.message)
+          delay_seconds = float(delay.group(1))
+          time.sleep(delay_seconds)
+          post()
+  except:
+    errors = errors + 1
+    if (errors > 5):
+      print("Too many errors")
+      exit()
 post()
